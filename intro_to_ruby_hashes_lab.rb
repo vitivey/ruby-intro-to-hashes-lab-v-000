c0343c6d8dbbfcdb89b237633f02d18409e9b15a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 13:54:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,6 +6,7 @@ def actor
  hash={
    actor: "Dwayne The Rock Johnson"
  }
+ hash
 end
 
 def monopoly
@@ -15,12 +16,21 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+  monopoly = {railroads: 
+                  {pieces: 4, 
+                    names: {}, 
+                    rent_in_dollars: {}}
+                }
 
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+  monopoly = {railroads: 
+                  {pieces: 4,
+                     names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad:{}, shortline:{}}, 
+                     rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}
+                }
 end
 
 def monopoly_with_fourth_tier
